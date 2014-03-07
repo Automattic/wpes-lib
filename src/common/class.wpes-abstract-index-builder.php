65fb443e8bcf705e3c2ab1a904b4e56b282a7f41
gibrown <greg@automattic.com>
2014-03-07 02:00:27 +0000

    Abstract index builder: remove extraneous functions.
    Not used.

--- src/common/class.wpes-abstract-index-builder.php
+++ src/common/class.wpes-abstract-index-builder.php
@@ -3,18 +3,15 @@
 require_once dirname( __FILE__ ) . '/class.wpes-analyzer-builder.php';
 
 abstract class WPES_Abstract_Index_Builder {
-	var $doctype2builder = array();
-
-	public function get_doc_builder( $type = false ) {
-		if ( false === $type )
-			return $this->doctype2builder;
-		
-		return $this->doctype2builder[$type];
-	}
-
+	 //The full config for the index (settings and mappings)
 	abstract public function get_config( $args );
-	abstract public function get_settings( $args );
-	abstract public function get_mappings( $args );
-	abstract public function get_doc_callbacks();
 
+	 //Index settings: shards, repliacs, analyzers
+	abstract public function get_settings( $args );
+
+	//document mappings
+	abstract public function get_mappings( $args );
+	
+	 //Document Name to Document Builder Mappings
+	abstract public function get_doc_callbacks();
 }
